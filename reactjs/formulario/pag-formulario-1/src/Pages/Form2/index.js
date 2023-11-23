dca5254b4c27e37a0e0678a4c87eaaf8d0d1fccd
Andre-R0cha <105022036+Andre-R0cha@users.noreply.github.com>
2023-11-23 09:06:06 -0300

    teste de css

--- reactjs/formulario/pag-formulario-1/src/Pages/Form2/index.js
+++ reactjs/formulario/pag-formulario-1/src/Pages/Form2/index.js
@@ -1,149 +1,139 @@
-import React, {useState} from 'react';
-import { styles } from './style';
-import UserService from '../../Services/login/UserService';
-import { validarEmail, validarNome, validarSenha, validarTelefone, validarConfirmarSenha } from '../../Utils/valid/validadores';
-import Input from '../../Components/Input';
-import Botao from '../../Components/Botao';
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { styles, Form, Title} from "./style";
+import UserService from "../../Services/login/UserService";
+import {
+  validarEmail,
+  validarNome,
+  validarSenha,
+  validarTelefone,
+  validarConfirmarSenha,
+} from "../../Utils/valid/validadores";
+import Input from "../../Components/Input";
+import Botao from "../../Components/Botao";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-
-
-
-
-
-
-
-
-
-
-const userService = new UserService()
-
+const userService = new UserService();
 
 const Cadastro = () => {
-  const [loading, setLoading] = useState()
-  const [form, setForm] = useState([])
-  const navigate = useNavigate()
+  const [loading, setLoading] = useState();
+  const [form, setForm] = useState([]);
+  const navigate = useNavigate();
 
+ 
 
-  const Title = styled.h1`
-  position: relative;
-  align-items: flex-start;
-
-  /* justify-content: flex-end; */
-  padding-bottom: 20px;
-  font-size:2.5em;
-  `;
-// const Input = styled.Input`
-// width: 20px;
-// `;
-
-
-
-  
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      setLoading(true)
+      setLoading(true);
       const { data } = await userService.cadastrar({
         nome: form.nome,
         telefone: form.telefone,
         email: form.email,
         password: form.password,
-      })
+      });
       if (data) {
         const responseLogin = await userService.login({
           email: form.email,
-          password: form.password
-        })
+          password: form.password,
+        });
         if (responseLogin === true) {
-          alert('usuário Cadastrado com Sucesso')
-          navigate('/home')
+          alert("usuário Cadastrado com Sucesso");
+          navigate("/home");
         }
+      }
+      setLoading(false);
+    } catch (err) {
+      alert("Algo deu errado com o Cadastro" + err);
     }
-      setLoading(false)
-    }
-    catch (err) {
-      alert('Algo deu errado com o Cadastro' + err)
-    }
-  }
+  };
 
   const handleChange = (event) => {
-    setForm({...form, [event.target.name]: event.target.value})
-  }
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
 
   const validadorInput = () => {
-    return validarEmail(form.email) 
-    && validarSenha(form.password)
-    && validarTelefone(form.telefone)
-    && validarConfirmarSenha(form.password, form.confirmarPassword)
-    && validarNome(form.nome)
-  }
+    return (
+      validarEmail(form.email) &&
+      validarSenha(form.password) &&
+      validarTelefone(form.telefone) &&
+      validarConfirmarSenha(form.password, form.confirmarPassword) &&
+      validarNome(form.nome)
+    );
+  };
 
   return (
-    <div style={styles.container}>
-   <form>
-        <Title >Faça o seu Cadastro</Title>
-        <Input
-        style={styles.input}
-          name='nome'
-          placeholder='Digite o seu nome'
-          onChange={handleChange}
-          type='text'
-        />
-        <Input
-   style={styles.input}
-          name='telefone'
-          placeholder='Digite o seu telefone'
-          onChange={handleChange}
-          type='number'
-        />
-        <Input
-        style={styles.input}
-        
-          name='email'
-          placeholder='Digite o seu e-mail'
-          onChange={handleChange}
-          type='email'
-        />
-        <Input
-        style={styles.input}
-          name='password'
-          placeholder='Digite a sua senha'
-          onChange={handleChange}
-          type='password'
-        />
-        <Input
-style={styles.input}
-          name='confirmarPassword'
-          placeholder='Confirme a sua senha'
-          onChange={handleChange}
-          type='password'
-        />
-        {/* <Botao
-        style={styles.button}
-          type='submit'
-          text='Efetuar Cadastro!'
-          onClick={handleSubmit}
-          disabled={loading === true || !validadorInput()}
-        /> */}
-        {/* <SubContainerSign>
-          <p>Já possui conta?</p>
-          <NavLink to="*">Login</NavLink>
-        </SubContainerSign> */}
-               <button
-          style={styles.button}
-          type='submit'
-          onClick={handleSubmit}
-          disabled={loading === true || !validadorInput()}
-        >
-          Efetuar Cadastro!
-        </button>
-    </form>
-    </div>
-    
-  )
-}
+    <>
+      {/* <AnimatedGradient> */}
+        <div style={styles.container}>
+            <Title>Faça o seu Cadastro</Title>
+          <Form>
 
+            <Input
+              style={styles.input}
+              name="nome"
+              placeholder="Digite o seu nome"
+              onChange={handleChange}
+              type="text"
+            />
+
+            <Input
+              style={styles.input}
+              name="telefone"
+              placeholder="Digite o seu telefone"
+              onChange={handleChange}
+              type="number"
+            />
+
+            <Input
+              style={styles.input}
+              name="email"
+              placeholder="Digite o seu e-mail"
+              onChange={handleChange}
+              type="email"
+            />
+
+            <Input
+              style={styles.input}
+              name="password"
+              placeholder="Digite a sua senha"
+              onChange={handleChange}
+              type="password"
+            />
+
+            <Input
+              style={styles.input}
+              name="confirmarPassword"
+              placeholder="Confirme a sua senha"
+              onChange={handleChange}
+              type="password"
+            />
+
+            <Botao
+        style={styles.button}
+        type='submit'
+        text='Efetuar Cadastro!'
+        onClick={handleSubmit}
+        disabled={loading === true || !validadorInput()}
+        />
+           
+          <p>Já possui conta?</p>
+          {/* <NavLink to="*">Login</NavLink> */}
+    
+            {/* <button
+              style={styles.button}
+              type="submit"
+              onClick={handleSubmit}
+              disabled={loading === true || !validadorInput()}
+            >
+              Efetuar Cadastro!
+            </button> */}
+            <div id="animatedGradient"></div>
+          </Form>
+        </div>
+      {/* </AnimatedGradient> */}
+    </>
+  );
+};
 
 export default Cadastro;
--- reactjs/formulario/pag-formulario-1/src/Pages/Form2/style.js
+++ reactjs/formulario/pag-formulario-1/src/Pages/Form2/style.js
@@ -1,48 +1,143 @@
+import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 
+const colors = {
+  primary: "#3498db",
+  secundary: "#3366FF",
+  background: " rgb(0, 179, 182)",
+  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+  // cor-primaria: '#3498db', /* Azul */
+  // cor-secundaria: '#e74c3c', /* Vermelho */
+  // cor-terciaria: '#2ecc71', /* Verde */
+  // cor-quaternaria: '#f39c12', /* Laranja */
+  // cor-quintenaria: '#9b59b6', /* Roxo */
+  // cor-sextenaria: '#34495e', /* Cinza Azulado */
+  // cor-base: '#ecf0f1', /* Cinza Claro */
 
-export const styles = {
-
-
-
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    margin: '5% 35%',  
-    padding: '5%',
-    backgroundColor: 'rgba(200, 0, 100, 0.1)',
-    borderRadius: '30px',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    width: '300px',
-  },
-
-
-  // title: {
-  //   position: 'relative',
-  //   alignItems: 'flex-end',
-  //   // width: '1px',
-  //   // height: '120px',
-  //   fontSize: '2rem',
-  //   marginBottom: '30px',
-   
-  // },
-  input: {
-    marginBottom: '100px',
-    padding: '8px',
-    width: '100%',
-    border: '5px solid #ccc',
-    borderRadius: '4px',
-    boxSizing: 'border-box',
-  },
-  button: {
-    width: '100%',
-    padding: '8px',
-    backgroundColor: '#007bff',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
-  },
+  // cor1: '#ff3b30', /* Vermelho */
+  // cor2: '#ff9500', /* Laranja */
+  // cor3: '#ffcc00', /* Amarelo */
+  // cor4: '#4cd964', /* Verde */
+  // cor5: '#5ac8fa', /* Azul */
+  // cor6: '#007aff', /* Azul Claro */
+  // cor7: '#5856d6', /* Roxo */
+  // cor8: '#ff2d55', /* Rosa */
 };
 
-// var 
+export const Title = styled.h1`
+  position: absolute;
+  top: 120px;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: justify;
+  padding-bottom: 10px;
+  font-size: 32px;
+`;
+
+export const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    margin: "2% 35%",
+    padding: "100px",
+    backgroundColor: colors.background,
+    borderRadius: "30px",
+    boxShadow: colors.boxShadow,
+    width: "300px",
+    zIndex: "-1",
+  },
+
+};
+
+export const Form = styled.form`
+  /* h1 {
+    position: relative;
+    width: 100%;   
+    padding: 2px;
+    color: white;
+    text-align: flex-end;
+    font-size: 32px;
+    font-weight: light;
+  } */
+  input {
+    display: block;
+    margin: 0 auto;
+    padding: 10px;
+    margin-top: 20px;
+    margin-bottom: 1px;
+  }
+
+  button {
+    display: block;
+    width: 200px;
+    padding: 5px;
+    margin-left: 40px;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    text-align: center;
+    background-color: #34495e;
+    &:hover,
+    a:hover {
+      background-color: blue;
+      color: green;
+      text-transform: capitalize;
+      border-radius: 10px;
+      border: none;
+    }
+  }
+  /* button:hover{
+    background-color: blue;
+    text-color:green;
+
+  } */
+
+  p {
+    margin: 20px 40px 0 50px;
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  a {
+    color: white;
+    font-size: 14px;
+  }
+`;
+
+export const AnimatedGradient = () => {
+  const [background, setBackground] = useState(getRandomGradient);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBackground(getRandomGradient());
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function getRandomGradient() {
+    const color1 = getRandomColor();
+    const color2 = getRandomColor();
+    return `linear-gradient(to right, ${color1}, ${color2})`;
+    //  `linear-gradient(to botton, ${color1}, ${color2})`
+    //   `linear-gradient(to botton right, ${color1}, ${color2})`
+    //   `linear-gradient(circle, ${color1}, ${color2})`;
+  }
+
+  function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16); // efeito de mudar o gradiente
+  }
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        background: background,
+        transition: "background 1s ease-in-out",
+      }}
+    />
+  );
+};
